d9da868b892e9ad393eea222e63c78033e396f51
Satoru SATOH <satoru.satoh@gmail.com>
2015-10-01 18:11:33 +0900

    add app initialization code

---
+++ app/__init__.py
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2015 Satoru SATOH <ssato@redhat.com>
+# License: MIT
+#
+from __future__ import absolute_import
+
+import flask
+import flask.ext.bootstrap
+import flask.ext.wtf.csrf
+
+import config
+
+
+def create_app(cnf_name="development"):
+    """
+    :param cnf_name: Config name, e.g. "development"
+    """
+    cnf = config.CNF[cnf_name]
+
+    app = flask.Flask(__name__)
+    app.config.from_object(cnf)
+    cnf.init_app(app)
+
+    bootstrap = flask.ext.bootstrap.Bootstrap()
+    bootstrap.init_app(app)
+
+    csrf = flask.ext.wtf.csrf.CsrfProtect()
+    csrf.init_app(app)
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
+
+# vim:sw=4:ts=4:et:
